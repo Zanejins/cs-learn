feb8ffcf085f46bee09d68abb14f1b5eb4923e7c
zanejins <zanejins@163.com>
2022-10-19 15:32:26 +0800

    others

--- Cpp/basic-knowledge/function/lotto.cpp
+++ Cpp/basic-knowledge/function/lotto.cpp
@@ -24,4 +24,13 @@ int main() {
 	return 0;
 }
 
-long double probability(unsigned numbers,)
+long double probability(unsigned numbers,unsigned picks) {
+	long double result = 1.0;
+	long double n;
+	unsigned p;
+	
+	for(n=numbers,p=picks;p>0;n--,p--) {
+		result = result * n / p; 
+	}
+	return result;
+}
